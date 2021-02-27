73945ea0bb86786ef33e157225032cc0763a55b9
Vishal Patel <vis142022@gmail.com>
2021-02-26 20:54:01 -0600

    added string functionality to test branches

--- main.java
+++ main.java
@@ -10,11 +10,13 @@ class myObj
 {
     // instance fields
     private int myInt;
+    private String myString;
 
     // constructors
-    public myObj(int myVar)
+    public myObj(int myVar, String myString)
     {
         this.myInt = myVar;
+        this.myString = myString;
     }
 
     // accessors
@@ -22,10 +24,20 @@ class myObj
     {
         return myInt;
     }
+    
+    public String getString()
+    {
+        return myString;
+    }
 
     // mutators
     public void setInt(int newInt)
     {
         myInt = newInt;
+    }
+    
+    public void setString(String newString)
+    {
+        myString = newString;
     }
 }
