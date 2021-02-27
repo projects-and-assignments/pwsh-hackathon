d3142cf2c2a8e81fccd60aee2ebdcd7a6c4084ea
Vishal Patel <vis142022@gmail.com>
2021-02-26 20:38:53 -0600

    boilerplate complete

--- main.java
+++ main.java
@@ -1,7 +1,31 @@
-public class main
+class main
 {
     public static void main(String[] args)
     {
         System.out.println("I'm ready for the competition!");
+    }
+}
+
+class myObj 
+{
+    // instance fields
+    private int myInt;
+
+    // constructors
+    public myObj(int myVar)
+    {
+        this.myInt = myVar;
+    }
+
+    // accessors
+    public int getInt()
+    {
+        return myInt;
+    }
+
+    // mutators
+    public void setInt(int newInt)
+    {
+        myInt = newInt;
     }
 }
